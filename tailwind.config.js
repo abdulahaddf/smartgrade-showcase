7e970a578de0f1b99fc37163c91f604626d5e112
abdulahaddf <abdulahad.df1@gmail.com>
2024-01-11 03:34:03 +0600

    initial

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,12 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-	content: ["./dist/*.html"],
-	theme: {
-		extend: {
-			container: {
-				center: true,
-				padding: "1.25rem",
-			},
-			colors: {
-				clifford: "#da373d",
-			},
-		},
-	},
-	plugins: [],
-};
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
